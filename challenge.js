e2e5f4a90a8f40f12c9d40628cbbe4560d62e203
Rayyan365 <rayanabdallah09@gamil.com>
2023-07-24 02:10:37 -0700

    largest-of-three done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let a=7;
+let b=8;
+let c=9;
+if (a>b && a>c)
+console.log (a + "is the largest numeber");
+else if (b>a && b>c)
+console.log (b + "is the largest number")
+else console.log (c+ "is the largest number")
